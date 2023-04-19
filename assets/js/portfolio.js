e099a4664b5114881a3fe1b50f635edaee80fdc9
Ekene <ekene2003@users.noreply.github.com>
2023-04-19 12:14:38 +0100

    new feature

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -11,6 +11,7 @@ let toogleBtn = singleelem(".circle");
 let openBtn =  singleelem(".nav-icon");
 let closeBtn = singleelem(".nav-icon2");
 let nav = singleelem(".nav-wrap");
+let darkBg = singleelem(".bg-section");
 openBtn.onclick = () => {
      openBtn.style.display = "none";
      nav.style.left = "0";
@@ -19,15 +20,22 @@ openBtn.onclick = () => {
 closeBtn.onclick = () => {
      closeBtn.style.display = "none";
      openBtn.style.display = "block";
-
      nav.style.left = "-100%";
 }
 darkModeBtn.onclick = () => {
      toogleBtn.style.right = "0%";
      toogleBtn.style.left = "";
+     darkBg.innerHTML = `
+     <img src="./assets/images/night2.jpg" alt="" class="bg">
+     `
+     darkBg.style.transition = "all 1s ease-in-out";
 }
 lightModeBtn.onclick = () => {
      toogleBtn.style.left = "0%";
      toogleBtn.style.right = "";
-      
+     darkBg.innerHTML = `
+               <img src="./assets/images/pexels-pixabay-514575.jpg" alt="" class="bg">
+     `;
+     darkBg.style.transition = "all 1s ease-in-out";
+
 }
