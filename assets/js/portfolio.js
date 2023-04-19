f96fde59ded3d7883815e69a65de9ab778c1e281
Ekene <ekene2003@users.noreply.github.com>
2023-04-19 13:01:05 +0100

    added darkmode

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -12,6 +12,16 @@ let openBtn =  singleelem(".nav-icon");
 let closeBtn = singleelem(".nav-icon2");
 let nav = singleelem(".nav-wrap");
 let darkBg = singleelem(".bg-section");
+let aboutSection = singleelem(".about-section");
+let skillSection = singleelem(".skill-section");
+let projectSection = singleelem(".project-section");
+let footer = singleelem(".footer");
+let aboutHead = singleelem(".about-head");
+let projectHead = singleelem(".project-head");
+let projectBtn = multielem(".project-btn");
+let footerLinks = multielem(".footer-links");
+let socials = singleelem(".socials");
+let footerHead = singleelem(".get-in-touch");
 openBtn.onclick = () => {
      openBtn.style.display = "none";
      nav.style.left = "0";
@@ -27,8 +37,24 @@ darkModeBtn.onclick = () => {
      toogleBtn.style.left = "";
      darkBg.innerHTML = `
      <img src="./assets/images/night2.jpg" alt="" class="bg">
-     `
-     darkBg.style.transition = "all 1s ease-in-out";
+     `;
+     darkBg.style.transition = "all 2s ease-in-out 5ms";
+     aboutSection.style.background = "#121212";
+     projectSection.style.background = "#121212";
+     footer.style.background = "#121212";
+     skillSection.style.background = "lightgrey";
+     socials.style.boxShadow = "none"; 
+     socials.style.background = "black"; 
+     projectBtn.forEach(btn => {
+     btn.style.color = "white"; 
+     });
+     aboutHead.style.color = "white";
+     projectHead.style.color = "white";
+     footerHead.style.color = "white";
+     footerLinks.forEach(link => {
+     link.style.color = "white";
+     });
+
 }
 lightModeBtn.onclick = () => {
      toogleBtn.style.left = "0%";
@@ -36,6 +62,21 @@ lightModeBtn.onclick = () => {
      darkBg.innerHTML = `
                <img src="./assets/images/pexels-pixabay-514575.jpg" alt="" class="bg">
      `;
-     darkBg.style.transition = "all 1s ease-in-out";
+     darkBg.style.transition = "all 2s ease-in-out 5ms";
+     aboutSection.style.background = "#fbf8f5";
+     projectSection.style.background = "#fbf8f5";
+     footer.style.background = "#fbf8f5";
+     skillSection.style.background = "#eeeeeb";
+     socials.style.boxShadow = "2px 2px 6px 3px gray"; 
+     socials.style.background = "black"; 
+     projectBtn.forEach(btn => {
+     btn.style.color = "black"; 
+     });
+     aboutHead.style.color = "black";
+     projectHead.style.color = "black";
+     footerHead.style.color = "black";
+     footerLinks.forEach(link => {
+     link.style.color = "black";
+     });
 
 }
